fc49815ae6bb78e8cbfee972be8c651a7e1e4244
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-05-24 01:24:23 +0200

    commit

--- NetCoreBootcampT23MVC/Cliente.cs
+++ NetCoreBootcampT23MVC/Cliente.cs
@@ -10,12 +10,14 @@ namespace NetCoreBootcampT23MVC_EX1
         public const int MAXAPELLIDOS = 250;
         public const int MAXDIRECCION = 250;
         public const int TOTALDNI = 11;
+
         public int? Id { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string Direccion { get; set; }
         public int? DNI { get; set; }
         public DateTime? Fecha { get; set; }
+
 
         public override string ToString()
         {
